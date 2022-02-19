f460dffbe619768e320bfdcd2290692bf05a27fa
sunghyuki <sunghyuk1609@gmail.com>
2022-02-19 23:44:51 +0900

    feat : Point 클래스 생성 - 첫 번째 Point의 left 상태는 false인 메서드 추가 - 이전 Point의 right 상태에 따라 현재 Point의 right 상태를 결정하는 메서드 추가 - 마지막 Point의 right 상태는 false인 메서드 추가

---
+++ src/main/java/domain/Point.java
@@ -0,0 +1,38 @@
+package domain;
+
+public class Point {
+    private static final String DUPLICATED_LINE_ERROR_MESSAGE = "[ERROR] 다리는 연속되어 존재할 수 없습니다.";
+
+    private boolean left;
+    private boolean right;
+
+    public Point() {
+    }
+
+    public Point(boolean left, boolean right) {
+        validateDuplicatedLine(left, right);
+        this.left = left;
+        this.right = right;
+    }
+
+    public Point first() {
+        return new Point(false, RandomPointGenerator.generate());
+    }
+
+    public Point next(Point prePoint) {
+        if (prePoint.right) {
+            return new Point(prePoint.right, false);
+        }
+        return new Point(false, RandomPointGenerator.generate());
+    }
+
+    public Point last(Point prePoint) {
+        return new Point(prePoint.right, false);
+    }
+
+    private void validateDuplicatedLine(boolean left, boolean right) {
+        if (left && right) {
+            throw new IllegalArgumentException(DUPLICATED_LINE_ERROR_MESSAGE);
+        }
+    }
+}
