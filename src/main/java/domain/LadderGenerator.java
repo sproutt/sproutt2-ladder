5704fab8f3ded1f745d8e505661f139d4512338d
sunghyuki <sunghyuk1609@gmail.com>
2022-02-20 17:35:08 +0900

    feat : LadderGenerator 클래스 생성 - 참여할 사람의 수와 사다리 높이를 입력받아 사다리를 만들어주는 메서드 추가

---
+++ src/main/java/domain/LadderGenerator.java
@@ -0,0 +1,15 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LadderGenerator {
+
+    public static Ladder generate(int ladderHeight, int playerCount) {
+        List<Line> lines = new ArrayList<>();
+        for (int i = 0; i < ladderHeight; i++) {
+            lines.add(LineGenerator.generate(playerCount));
+        }
+        return new Ladder(lines);
+    }
+}
