02a7b25544752c7537b64866cf66f4db84cbc090
sunghyuki <sunghyuk1609@gmail.com>
2022-02-20 17:36:22 +0900

    refactor : Line 생성과 Ladder 생성 기능 다른 클래스에 위임

--- src/main/java/controller/Game.java
+++ src/main/java/controller/Game.java
@@ -1,18 +1,13 @@
 package controller;
 
 import domain.Ladder;
-import domain.Line;
-import domain.Point;
+import domain.LadderGenerator;
 import view.InputView;
 import view.OutputView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Game {
     private InputView inputView;
     private OutputView outputView;
-    private List<Point> points;
 
     public Game() {
         inputView = new InputView();
@@ -23,45 +18,12 @@ public class Game {
         int playerCount = inputView.inputPlayerCount();
         int ladderHeight = inputView.inputLadderHeight();
 
-        Ladder ladder = generateLadder(playerCount, ladderHeight);
+        Ladder ladder = LadderGenerator.generate(ladderHeight, playerCount);
+
         for (int i = 0; i < ladderHeight; i++) {
             outputView.printOneLine(ladder.getLines().get(i).toBoolean());
         }
 
 
-    }
-
-    public Line generateLine(int playerCount) {
-        if (playerCount == 1) {
-            Point onlyPoint = new Point();
-            onlyPoint = onlyPoint.first();
-            List<Point> points = new ArrayList<>();
-            points.add(onlyPoint);
-            return new Line(points);
-        }
-        Point prePoint = new Point();
-        prePoint = prePoint.first();
-        List<Point> points = new ArrayList<>();
-        points.add(prePoint);
-
-        Point presentPoint = new Point();
-        for(int i = 0; i < playerCount - 2; i++) {
-            presentPoint = new Point();
-            presentPoint = presentPoint.next(prePoint);
-            prePoint = presentPoint;
-            points.add(presentPoint);
-        }
-        Point lastPoint = new Point();
-        lastPoint = lastPoint.last(presentPoint);
-        points.add(lastPoint);
-        return new Line(points);
-    }
-
-    public Ladder generateLadder(int playerCount, int ladderHeight) {
-        List<Line> lines = new ArrayList<>();
-        for (int i = 0; i < ladderHeight; i++) {
-            lines.add(generateLine(playerCount));
-        }
-        return new Ladder(lines);
     }
 }
