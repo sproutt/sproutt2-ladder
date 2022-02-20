4747cc95c00b8fe3a460f04f0b389a0e55349abf
sunghyuki <sunghyuk1609@gmail.com>
2022-02-20 17:54:21 +0900

    feat : Name 클래스 생성 - 이름 값 포장을 위한 래퍼 클래스 - 이름 값 생성 규칙 예외 검증 메서드 추가

---
+++ src/main/java/domain/Name.java
@@ -0,0 +1,27 @@
+package domain;
+
+public class Name {
+    private static final String NAME_LENGTH_ERROR = "[ERROR] 이름은 5글자를 초과할 수 없습니다.";
+    private static final String EMPTY_NAME_ERROR = "[ERROR] 이름은 공백일 수 없습니다.";
+    private static final int MAX_NAME_LENGTH = 5;
+
+    private String name;
+
+    public Name(String name) {
+        validateEmptyName(name);
+        validateNameLength(name);
+        this.name = name;
+    }
+
+    private void validateEmptyName(String name) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException(EMPTY_NAME_ERROR);
+        }
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException(NAME_LENGTH_ERROR);
+        }
+    }
+}
