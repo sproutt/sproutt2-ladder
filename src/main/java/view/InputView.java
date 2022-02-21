eee1aa3f2f21af00c7ad158ae941d5e20b2a8aea
sunghyuki <sunghyuk1609@gmail.com>
2022-02-21 19:54:47 +0900

    feat : InputView 클래스 수정 - View의 역할은 구분자를 분리하고, 공백을 제거해주는 등과 같은 데이터를 최소한의 정리된 형태로 만들어, Controller로 넘겨주는 것까지라고 생각된다. 그래서 예외 처리는 Controller에서 해야된다고 생각한다. - 참여할 사람 이름 입력받기 메서드 추가 - 사다리 높이 입력받기 메서드 추가 - 실행 결과 이름 입력받기 메서드 추가 - 문자열 처리 메서드 추가 - 결과를 보고 싶은 사람 입력받기 메서드 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,19 +1,52 @@
 package view;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
+    private static final String INPUT_PLAYER_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String DELIMITER = ",";
+    private static final String INPUT_GAME_RESULT_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
+    private static final String INPUT_LADDER_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
+    private static final String INPUT_PLAYER_NAME_MESSAGE = "결과를 보고 싶은 사람은?";
+
     private Scanner scanner;
 
     public InputView() {
         scanner = new Scanner(System.in);
     }
 
-    public int inputPlayerCount() {
-        return scanner.nextInt();
+    public List<String> inputPlayer() {
+        System.out.println(INPUT_PLAYER_MESSAGE);
+        List<String> playerNames = splitByDelimiter(scanner.nextLine());
+        return trim(playerNames);
     }
 
     public int inputLadderHeight() {
-        return scanner.nextInt();
+        System.out.println(INPUT_LADDER_HEIGHT_MESSAGE);
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    private List<String> splitByDelimiter(String playerNames) {
+        return Arrays.asList(playerNames.split(DELIMITER));
+    }
+
+    private List<String> trim(List<String> playerNames) {
+        return playerNames.stream()
+                .map(String::trim)
+                .collect(Collectors.toList());
+    }
+
+    public List<String> inputGameResult() {
+        System.out.println(INPUT_GAME_RESULT_MESSAGE);
+        List<String> gameResults = splitByDelimiter(scanner.nextLine());
+        return trim(gameResults);
+    }
+
+    public String inputPlayerName() {
+        System.out.println(INPUT_PLAYER_NAME_MESSAGE);
+        return scanner.nextLine();
     }
 }
