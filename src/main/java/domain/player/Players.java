e251f6caf39d39fb16c875797eaadad1ee150b8f
sunghyuki <sunghyuk1609@gmail.com>
2022-02-21 19:38:53 +0900

    feat : List<Player>를 가지고 있는 일급 컬렉션 생성 - player 객체들의 이름을 반환해주는 메서드 추가 - 모든 플레이어들을 완주하게 하는 메서드 추가

---
+++ src/main/java/domain/player/Players.java
@@ -0,0 +1,35 @@
+package domain.player;
+
+import domain.Line.Line;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Players {
+
+    private List<Player> players;
+
+    public Players(List<Player> players) {
+        this.players = new ArrayList<>(players);
+    }
+
+    public List<Player> getPlayers() {
+        return Collections.unmodifiableList(players);
+    }
+
+    public List<String> toPlayerNames() {
+        return players.stream()
+                .map(player -> player.getName())
+                .collect(Collectors.toList());
+    }
+
+    public int size() {
+        return players.size();
+    }
+
+    public void moveAll(List<Line> lines) {
+        players.forEach(player -> player.moveAll(lines));
+    }
+}
