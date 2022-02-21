0d9cc9e264192272f208b7294b7a03ab98aed09f
sunghyuki <sunghyuk1609@gmail.com>
2022-02-21 19:58:45 +0900

    feat : Game 시작 컨트롤러 메서드 추가

--- src/main/java/controller/Game.java
+++ src/main/java/controller/Game.java
@@ -1,9 +1,17 @@
 package controller;
 
-import domain.Ladder;
-import domain.LadderGenerator;
+import common.PlayerGameResult;
+import domain.gameresult.GameResultFactory;
+import domain.gameresult.GameResults;
+import domain.ladder.Ladder;
+import domain.ladder.LadderGenerator;
+import domain.player.Players;
+import domain.player.PlayersFactory;
 import view.InputView;
 import view.OutputView;
+
+import java.util.List;
+import java.util.Map;
 
 public class Game {
     private InputView inputView;
@@ -15,11 +23,17 @@ public class Game {
     }
 
     public void play() {
-        int playerCount = inputView.inputPlayerCount();
+        Players players = PlayersFactory.from(inputView.inputPlayer());
+        List<String> gameResultNames = inputView.inputGameResult();
         int ladderHeight = inputView.inputLadderHeight();
+        GameResults gameResults = GameResultFactory.from(gameResultNames, ladderHeight);
 
-        Ladder ladder = LadderGenerator.generate(ladderHeight, playerCount);
+        Ladder ladder = LadderGenerator.generate(ladderHeight, players.size());
+        players.moveAll(ladder.getLines());
 
+        Map<String, String> matchedGameResult = PlayerGameResult.match(players, gameResults);
+
+        outputView.printPlayers(players.toPlayerNames());
         for (int i = 0; i < ladderHeight; i++) {
             outputView.printOneLine(ladder.getLines().get(i).toBoolean());
         }
