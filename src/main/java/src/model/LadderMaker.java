2ee7b935ddae990bf0861b26eaf3e1cd0be25ec6
kanamycine <jinyeong5320@gmail.com>
2022-02-24 21:47:01 +0900

    refactor : 이중 for문 제거하기 - makeLine() : 2차원 배열 취급 -> 1차원 배열 생성 역할 위임 - makeBridge() : 조건에 따라 다리 생성 로직 분리

--- src/main/java/src/model/LadderMaker.java
+++ src/main/java/src/model/LadderMaker.java
@@ -5,28 +5,17 @@ import java.util.Random;
 public class LadderMaker {
 	private int height;
 	private int playerNumber;
+
 	public LadderMaker(int height, int playerNumber) {
 		this.height = height;
 		this.playerNumber = playerNumber;
 	}
 
-	public int[][] makeLadder(){
-		int[][] ladderBlueprint = new int[height][(2*playerNumber)-1];
-		Random random = new Random();
+	public int[][] makeLadder() {
+		int[][] ladderBlueprint = new int[height][(2 * playerNumber) - 1];
 
 		for (int i = 0; i < ladderBlueprint.length; i++) {
-			for (int j = 0; j < ladderBlueprint[i].length; j++) {
-				ladderBlueprint[i][j] = 0;
-				if (j == 1) {
-					ladderBlueprint[i][j] = random.nextInt(2);
-				}
-				if (j>1 && j%2 == 1 && ladderBlueprint[i][j-2] == 1) {
-					ladderBlueprint[i][j] = 0;
-				}
-				if (j>1 && j%2 == 1 && ladderBlueprint[i][j-2] == 0) {
-					ladderBlueprint[i][j] = random.nextInt(2);
-				}
-			}
+			ladderBlueprint[i] = makeLine();
 		}
 		return ladderBlueprint;
 	}
