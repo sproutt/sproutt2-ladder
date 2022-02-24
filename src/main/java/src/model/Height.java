6daa5b3ebac414653804da6ae541e5c483a9bd8d
kanamycine <jinyeong5320@gmail.com>
2022-02-24 23:07:21 +0900

    feat : Height 원시값 포장 - (int height) -> (Height Height)

---
+++ src/main/java/src/model/Height.java
@@ -0,0 +1,23 @@
+package src.model;
+
+public class Height {
+
+	private static final String UNDER_ZERO_NUMBER_ERROR_MESSAGE = "[ERROR] 높이는 양수를 입력해야 한다.";
+	public static final int MINIMUM_HEIGHT = 1;
+	private int height;
+
+	public Height(int height) {
+		validateNegative(height);
+		this.height = height;
+	}
+
+	private void validateNegative(int height) {
+		if (height < MINIMUM_HEIGHT) {
+			throw new IllegalArgumentException(UNDER_ZERO_NUMBER_ERROR_MESSAGE);
+		}
+	}
+
+	public int getHeight() {
+		return height;
+	}
+}
--- src/main/java/src/model/LadderMaker.java
+++ src/main/java/src/model/LadderMaker.java
@@ -3,16 +3,16 @@ package src.model;
 import java.util.Random;
 
 public class LadderMaker {
-	private int height;
+	private Height height;
 	private int playerNumber;
 
 	public LadderMaker(int height, int playerNumber) {
-		this.height = height;
+		this.height = new Height(height);
 		this.playerNumber = playerNumber;
 	}
 
 	public int[][] makeLadder() {
-		int[][] ladderBlueprint = new int[height][(2 * playerNumber) - 1];
+		int[][] ladderBlueprint = new int[height.getHeight()][(2 * playerNumber) - 1];
 
 		for (int i = 0; i < ladderBlueprint.length; i++) {
 			ladderBlueprint[i] = makeLine();
