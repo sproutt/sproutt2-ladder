6b899b8960a9c0ddba334851390bcddac4694b17
kanamycine <jinyeong5320@gmail.com>
2022-02-25 22:25:43 +0900

    feat : 각 좌표 값 Bridge로 원시값 포장

---
+++ src/main/java/src/model/ladder/Bridge.java
@@ -0,0 +1,13 @@
+package src.model.ladder;
+
+public class Bridge {
+	private int value;
+
+	public Bridge(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+}
