891cd37254fcca4587d53e1039538c6fb6093224
kanamycine <jinyeong5320@gmail.com>
2022-02-27 22:34:15 +0900

    feat : ResultAllDto - resultAll dto 구현 Map<Player, ExecutionResult> -> Map <String, String>

---
+++ src/main/java/src/dto/ResultAllDto.java
@@ -0,0 +1,26 @@
+package src.dto;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import src.model.excutionResults.ExecutionResult;
+import src.model.player.Player;
+
+public class ResultAllDto {
+	private Map<String, String> resultAll;
+
+	private ResultAllDto(Map<String, String> resultAll){
+		this.resultAll = resultAll;
+	}
+
+	public static ResultAllDto from(Map<Player, ExecutionResult> results){
+		Map<String, String> resultAll = new LinkedHashMap<>();
+		for (Player player : results.keySet()) {
+			String playerName = player.getName();
+			ExecutionResult executionResult = results.get(player);
+			String resultName = executionResult.getResult();
+			resultAll.put(playerName, resultName);
+		}
+		return new ResultAllDto(resultAll);
+	}
+}
