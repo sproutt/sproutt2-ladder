ed971218e070bf9621bff2969336ef009361fea2
sunghyuki <sunghyuk1609@gmail.com>
2022-02-28 00:21:06 +0900

    test : GameResultFactory 클래스 테스트 코드 작성 - 참여한 사람들의 수만큼 Player 객체를 생성한다.

---
+++ src/test/java/domain/gameresult/GameResultFactoryTest.java
@@ -0,0 +1,32 @@
+package domain.gameresult;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GameResultFactoryTest {
+
+    @Test
+    @DisplayName("참여한 사람들의 수만큼 실행 결과 객체를 생성한다.")
+    void testgeneratedGameResultsSize() {
+        // given
+        GameResultName name1 = new GameResultName("꽝");
+        GameResultName name2 = new GameResultName("20");
+
+        List<GameResultName> gameResultNames = new ArrayList<>();
+        gameResultNames.add(name1);
+        gameResultNames.add(name2);
+
+        int ladderHeight = 5;
+
+        // when
+        GameResults gameResults = GameResultFactory.from(gameResultNames, ladderHeight);
+
+        // when
+        assertThat(gameResults.size()).isEqualTo(2);
+    }
+}
