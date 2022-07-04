03e52cda70c524db8eab9c66badb29933d62ce53
mywnajsldkf <mywnajsldkf@gmail.com>
2022-07-04 17:11:12 +0900

    test : 이름 검증 테스트 추가 - 이름이 5글자 이상일 경우, Exception 발생 O - 이름이 5글자 이하인 경우, Exception 발생 X

--- src/test/java/ParticipantTest.java
+++ src/test/java/ParticipantTest.java
@@ -1,9 +1,25 @@
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class ParticipantTest {
     @Test
-    public void should_ThrowException_When_ParticipantName_over_5() {
-        Participant participant = new Participant();
+    public void should_throwException_when_participantName_over_5() {
+        // given
+        String testName = "crystal";
+
+        // when & then
+        assertThatThrownBy(() -> new Participant(testName))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void should_success_when_participantName_below_5() {
+        // given
+        String testName = "july";
+
+        // when & then
+        assertThatCode(() -> new Participant(testName)).doesNotThrowAnyException();
     }
 }
 
