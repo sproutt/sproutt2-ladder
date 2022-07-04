fa3dd28f15e5e3ebf2012ea5424ca26cb30caae7
mywnajsldkf <mywnajsldkf@gmail.com>
2022-07-04 17:24:29 +0900

    test : 실패 케이스 작성 - Winning 객체 없음

---
+++ src/test/java/WinningTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+public class WinningTest {
+    @Test
+    public void should_throw_exception_when_winning_input_is_wrong() {
+        Winning winning = new Winning();
+    }
+}
