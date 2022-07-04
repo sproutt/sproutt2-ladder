3afdd121257488dc3eb3106fcbdf76dd2803a3ce
mywnajsldkf <mywnajsldkf@gmail.com>
2022-07-04 15:57:49 +0900

    test : 실패 케이스 생성 - Participant 객체 없음

---
+++ src/test/java/ParticipantTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class ParticipantTest {
+    @Test
+    public void should_ThrowException_When_ParticipantName_over_5() {
+        Participant participant = new Participant();
+    }
+}
+
