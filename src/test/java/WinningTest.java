a1d56913c5c87863651ae6f8faa48e80ac550fac
mywnajsldkf <mywnajsldkf@gmail.com>
2022-07-07 16:21:32 +0900

    test : winning 객체 생성 테스트

--- src/test/java/WinningTest.java
+++ src/test/java/WinningTest.java
@@ -1,8 +1,19 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class WinningTest {
-    @Test
-    public void should_throw_exception_when_winning_input_is_wrong() {
-        Winning winning = new Winning();
+    @ParameterizedTest
+    @ValueSource(strings = {"광", "-3000"})
+    public void should_throw_exception_when_winning_input_is_wrong(String candidate) {
+        assertThatThrownBy(() -> new Winning(candidate))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"꽝", "3000"})
+    public void should_success_when_winning_input_is_correct(String candidate) {
+        assertThatCode(()-> new Winning(candidate)).doesNotThrowAnyException();
     }
 }
