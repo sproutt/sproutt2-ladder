016e50ec89e059a226be2eb076222649e6227769
mywnajsldkf <mywnajsldkf@gmail.com>
2022-07-12 17:35:10 +0900

    chores : import 패키지 변경

--- src/test/java/WinningTest.java
+++ src/test/java/WinningTest.java
@@ -1,5 +1,5 @@
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
 
 import static org.assertj.core.api.Assertions.*;
 
