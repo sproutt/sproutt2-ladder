6e2a61b40ceb301da097d450c1e6cc5a3ce98924
mywnajsldkf <mywnajsldkf@gmail.com>
2022-07-18 21:45:47 +0900

    feat : Line 객체 생성
    - 사다리를 구성하는 한줄(참가자 크기 사이즈)을 생성

--- src/main/java/Line.java
+++ src/main/java/Line.java
@@ -1,14 +1,49 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Line {
-    private static List<Point> oneLine;
+    private static final int NON_CONNECTION = 0;
+    private static final int CONNECTION = 1;
+    private static final int START_INDEX = 0;
 
-    public Line(List<Point> oneLine) {
-        this.oneLine = new ArrayList<>(oneLine);
+    private List<Point> line;
+
+    public Line(int participant) {
+
+        line = new ArrayList<>();
+
+        for (int i = 0; i < participant - 1; i++) {
+            line.add(new Point(isConnection(i)));
+        }
     }
 
-    public int getLineSize() {
-        return oneLine.size();
+    public List<Point> getLine() {
+        return line;
+    }
+
+    private boolean isConnection(int index) {
+        if (index == START_INDEX) {
+            return selectConnection();
+        }
+
+        boolean beforeConnection = (line.get(index - 1)).getConnection();
+
+        if (beforeConnection) {
+            return false;
+        }
+
+        return selectConnection();
+    }
+
+    private boolean selectConnection() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(2);
+        if (randomNumber == NON_CONNECTION) {
+            return false;
+        } else if (randomNumber == CONNECTION) {
+            return true;
+        }
+        return true;
     }
 }
