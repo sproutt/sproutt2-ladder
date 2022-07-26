b83f1cfabd096de248f4533c7234c8317dc0be21
mywnajsldkf <mywnajsldkf@gmail.com>
2022-07-26 14:49:21 +0900

    refactor : Participant -> Player 수정

---
+++ src/main/java/model/Player.java
@@ -0,0 +1,16 @@
+package model;
+public class Player extends Info {
+    private static final int MAX_NAME_SIZE = 5;
+    private static final String OVER_MAX_NAME_SIZE_EXCEPTION = "참여자 이름이 5글자 이상입니다.";
+
+    public Player(String name) {
+        validatePlayerNameSize(name);
+        this.info = name;
+    }
+
+    private void validatePlayerNameSize(String name) {
+        if (name.length() > MAX_NAME_SIZE) {
+            throw new RuntimeException(OVER_MAX_NAME_SIZE_EXCEPTION);
+        }
+    }
+}
