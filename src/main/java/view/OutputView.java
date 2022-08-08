f3ecde4c1cbd62af2c7c11b3ea57d240b0646fce
mywnajsldkf <mywnajsldkf@gmail.com>
2022-08-08 09:34:17 +0900

    feat : StringLadder 출력 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,9 +1,26 @@
 package view;
 
+import model.Ladder;
+
+import java.util.List;
+
 public class OutputView {
     private static final String OUTPUT_LADDER_RESULT = "사다리 결과";
 
-    public static void outputResultMessage() {
+    public static void printResultMessage() {
         System.out.println(OUTPUT_LADDER_RESULT);
+    }
+
+    public static void printStringLadder(List<List<String>> stringLadder) {
+        for (List<String> line : stringLadder) {
+            printStringLine(line);
+        }
+    }
+
+    private static void printStringLine(List<String> line) {
+        for (String stringLine : line) {
+            System.out.print(stringLine);
+        }
+        System.out.println();
     }
 }
